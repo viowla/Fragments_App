35bcb422b93835a449da919b72831f0f9dc5bc44
viowla <55015538+viowla@users.noreply.github.com>
2020-11-08 13:50:44 +0600

    edit gradle

--- app/src/main/java/com/example/fragmentsapp/adapter/PostAdapter.java
+++ app/src/main/java/com/example/fragmentsapp/adapter/PostAdapter.java
@@ -65,7 +65,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
 
     private void publisherInfo(ImageView image_profile, TextView username, TextView publisher, String userid){
 
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
 
 
     }
